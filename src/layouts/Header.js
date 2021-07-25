12b65890a555e871f102045306e59f4bf63274e7
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-07-25 15:24:46 +0200

    added Switch for header imgs

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "../styles/Header.css";
 import img1 from "../images/header1.jpg";
 import img2 from "../images/header2.jpg";
 import img3 from "../images/header3.jpg";
 
 const Header = () => {
-  return <img src={img1} alt="city" />;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact>
+          <img src={img1} alt="city" />
+        </Route>
+        <Route path="/products" render={() => <img src={img3} alt="city" />} />
+        <Route path="/contact" render={() => <img src={img2} alt="city" />} />
+        <Route path="/admin" render={() => <img src={img3} alt="city" />} />
+        <Route render={() => <img src={img1} alt="city" />} />
+      </Switch>
+    </>
+  );
 };
 
 export default Header;
