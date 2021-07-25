9320eef29057692f670c74ab29e89fec739f8108
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-07-25 14:55:14 +0200

    Added Navigation.js with stylings

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,4 +1,7 @@
 import React from "react";
+import img1 from "../images/header1.jpg";
+import img2 from "../images/header2.jpg";
+import img3 from "../images/header3.jpg";
 
 const Header = () => {
   return <div>Header</div>;
