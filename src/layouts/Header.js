0f6122272a2c33ac4386b408516dc7eb5d591a1c
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-07-25 15:13:27 +0200

    fixed header img to use object-fit

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/Header.css";
 import img1 from "../images/header1.jpg";
 import img2 from "../images/header2.jpg";
 import img3 from "../images/header3.jpg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <img src={img1} alt="city" />;
 };
 
 export default Header;
