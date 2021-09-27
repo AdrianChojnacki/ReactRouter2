77d7db5f5d0430f84e26331473c57a2e23cda6cc
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-09-27 22:35:07 +0200

    added useful things to Footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Route } from "react-router";
+import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h2>Stopka</h2>
+      <Route
+        path="/"
+        exact
+        render={(props) => (
+          <p>
+            Jesteś na <span>stronie głównej</span>
+          </p>
+        )}
+      />
+      <Route
+        path="/:page"
+        exact
+        render={(props) => (
+          <>
+            <p>
+              Jesteś na <span>{props.match.params.page}</span>
+            </p>
+            <p>
+              Jesteś na <span>{props.match.url}</span>
+            </p>
+            <p>
+              Jesteś na <span>{props.match.path}</span>
+            </p>
+          </>
+        )}
+      />
+      <Route
+        path="/:page/:idProduct"
+        exact
+        render={(props) => (
+          <>
+            <p>
+              Jesteś na{" "}
+              <span>
+                {props.match.params.page}===&gt;
+                {props.match.params.idProduct}
+              </span>
+            </p>
+            <p>
+              Jesteś na <span>{props.match.url}</span>
+            </p>
+            <p>
+              Jesteś na <span>{props.match.path}</span>
+            </p>
+          </>
+        )}
+      />
+    </div>
+  );
 };
 
 export default Footer;
