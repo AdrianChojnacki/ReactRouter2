cd3527a86d4549bb1ed6967d8bac5175c559fd64
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-09-27 20:47:03 +0200

    added some styles to Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "../styles/Article.css";
 
-export default function Article(props) {
+export default function Article({ title, author, text }) {
+  const styles = {
+    marginBottom: 10,
+    fontSize: 30,
+  };
+
   return (
     <article>
-      <h3>{props.title}</h3>
-      <span>{props.author}</span>
-      <p>{props.text}</p>
+      <h3 style={styles}>{title}</h3>
+      <span style={{ display: "block", marginBottom: 10, fontSize: 16 }}>{author}</span>
+      <p>{text}</p>
     </article>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,19 +6,19 @@ const articles = [
     id: 1,
     title: "Czym jest teoria strun?",
     author: "Jan Nowak",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ut fugiat vero eius voluptate, autem itaque numquam aspernatur natus assumenda accusamus ipsa? Similique assumenda autem eius iste porro corporis architecto.",
+    text: "1. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ut fugiat vero eius voluptate, autem itaque numquam aspernatur natus assumenda accusamus ipsa? Similique assumenda autem eius iste porro corporis architecto.",
   },
   {
     id: 2,
     title: "Czym jest paradoks fermiego?",
     author: "Jan Nowak",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ut fugiat vero eius voluptate, autem itaque numquam aspernatur natus assumenda accusamus ipsa? Similique assumenda autem eius iste porro corporis architecto.",
+    text: "2. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ut fugiat vero eius voluptate, autem itaque numquam aspernatur natus assumenda accusamus ipsa? Similique assumenda autem eius iste porro corporis architecto.",
   },
   {
     id: 3,
     title: "Czym jest materia i ciemna energia?",
     author: "Jan Nowak",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ut fugiat vero eius voluptate, autem itaque numquam aspernatur natus assumenda accusamus ipsa? Similique assumenda autem eius iste porro corporis architecto.",
+    text: "3. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae ut fugiat vero eius voluptate, autem itaque numquam aspernatur natus assumenda accusamus ipsa? Similique assumenda autem eius iste porro corporis architecto.",
   },
 ];
 
